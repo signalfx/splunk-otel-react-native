95ef5e745c54cc6f6433ad0fd1001ca0790692f6
Marten Hennoch <mhennoch@splunk.com>
2022-12-07 15:09:44 +0200

    Expose some convenience functions

--- src/splunkAttributeNames.ts
+++ src/splunkAttributeNames.ts
@@ -17,3 +17,5 @@ limitations under the License.
 export const SCREEN_NAME = 'screen.name';
 export const LAST_SCREEN_NAME = 'last.screen.name';
 export const COMPONENT = 'component';
+export const LOCATION_LATITUDE = 'location.lat';
+export const LOCATION_LONGITUDE = 'location.long';
