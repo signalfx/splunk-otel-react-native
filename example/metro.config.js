b83f55346e6d3a744ed6a35fa5f25a025ef1cea5
MartenH <72463136+mhennoch@users.noreply.github.com>
2023-10-12 13:02:22 +0300

    Upgrade example to 0.72.5 (#57)

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Splunk Inc.
+Copyright 2023 Splunk Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,36 +14,39 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
+const modules = Object.keys({ ...pak.peerDependencies });
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
-
-module.exports = {
-  projectRoot: __dirname,
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   watchFolders: [root],
 
-  // // We need to make sure that only one version is loaded for peerDependencies
-  // // So we block them at the root, and alias them to the versions in example's node_modules
-  // resolver: {
-  //   blacklistRE: exclusionList(
-  //     modules.map(
-  //       (m) =>
-  //         new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-  //     )
-  //   ),
+  // We need to make sure that only one version is loaded for peerDependencies
+  // So we block them at the root, and alias them to the versions in example's node_modules
+  resolver: {
+    blacklistRE: exclusionList(
+      modules.map(
+        (m) =>
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
+    ),
 
-  //   extraNodeModules: modules.reduce((acc, name) => {
-  //     acc[name] = path.join(__dirname, 'node_modules', name);
-  //     return acc;
-  //   }, {}),
-  // },
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
+  },
 
   transformer: {
     getTransformOptions: async () => ({
@@ -54,3 +57,5 @@ module.exports = {
     }),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
