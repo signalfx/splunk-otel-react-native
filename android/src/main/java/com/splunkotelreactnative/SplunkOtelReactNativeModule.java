0d79261e995d5f66288d6ea80a5d360639746a9c
mhennoch <mhennoch@splunk.com>
2023-11-06 13:29:01 +0200

    Fix incorrect type and wrong ios folder

--- android/src/main/java/com/splunkotelreactnative/SplunkOtelReactNativeModule.java
+++ android/src/main/java/com/splunkotelreactnative/SplunkOtelReactNativeModule.java
@@ -247,7 +247,7 @@ public class SplunkOtelReactNativeModule extends ReactContextBaseJavaModule {
 
       if (value instanceof String) {
         builder.put(entry.getKey(), (String) value);
-      } else if (value instanceof Integer) {
+      } else if (value instanceof Number) {
         // TODO fix this
         if (entry.getKey().equals("http.status_code")) {
           builder.put(entry.getKey(), ((Number) value).intValue());
