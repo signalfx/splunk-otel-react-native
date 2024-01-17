0d4f90c1f5f8a6fb3e97656f6cc662cfa4f89e29
MartenH <72463136+mhennoch@users.noreply.github.com>
2024-01-17 14:17:21 +0200

    Fix ios number conversion and upgrade otel (#70)

--- ios/ZipkinTransform.swift
+++ ios/ZipkinTransform.swift
@@ -111,6 +111,8 @@ struct ZipkinTransform {
         let spanId = otelSpan["id"] as? String ?? "0000000000000000"
         let name = otelSpan["name"] as? String ?? "unknown"
         let jsTags = otelSpan["tags"] as? Dictionary<String, Any> ?? [:]
+        let timestamp = anyToUInt64(otelSpan["timestamp"])
+        let duration = anyToUInt64(otelSpan["duration"])
 
         var tags: Dictionary<String, String> = [:]
 
@@ -134,8 +136,8 @@ struct ZipkinTransform {
                           id: spanId,
                           kind: otelSpan["kind"] as? String,
                           name: name,
-                          timestamp: otelSpan["timestamp"] as? UInt64 ?? 0,
-                          duration: otelSpan["duration"] as? UInt64 ?? 0,
+                          timestamp: timestamp,
+                          duration: duration,
                           remoteEndpoint: nil,
                           annotations: [],
                           tags: tags)
@@ -153,5 +155,28 @@ extension ZipkinSpan {
     
     func addEvent(name: String, timestamp: Date) {
         self.annotations.append(ZipkinAnnotation(timestamp: UInt64(timestamp.timeIntervalSince1970 * 1e6), value: name))
+    }
+}
+
+fileprivate func anyToUInt64(_ v: Any?) -> UInt64 {
+    if v == nil {
+        return 0
+    }
+
+    switch v {
+    case is Double:
+        let doubleValue = v as! Double
+        if (doubleValue >= 0.0) {
+            return UInt64(doubleValue)
+        }
+        return 0
+    case is Int:
+        let intValue = v as! Int
+        if (intValue >= 0) {
+            return UInt64(intValue)
+        }
+        return 0
+    default:
+        return 0
     }
 }
